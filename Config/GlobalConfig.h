cffe976ccdf29eaadbb6dcb55485ef9bc90ccb3d
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2025-01-27 20:45:53 +0800

    Added: Reading config file

---
+++ Config/GlobalConfig.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "../TypedefRepo.h"
+
+class GlobalConfig
+{
+public:
+    static GlobalConfig* getInstance();
+
+    static void initialize();
+    static void destroy();
+
+    void loadConfigFile(String& filename);				// Loads the config file
+	void printConfig() const;							// Prints the config
+
+    int getNumberOfThreads() const;
+    int getTargetNumber() const;
+
+private:
+	GlobalConfig();
+	static GlobalConfig* sharedInstance;
+
+    struct ConfigData
+    {
+        int numberOfThreads;
+        int targetNumber;
+    };
+
+    ConfigData configData;								// Config variable
+	bool parseConfigFile(String& line);					// Parses the config file
+};

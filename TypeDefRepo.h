875e15ca68b546f9fa1eda9de35aaad58bb41d36
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2025-01-31 23:46:09 +0800

    Added: intvector and threadvector

--- TypeDefRepo.h
+++ TypeDefRepo.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <thread>
 
 using namespace std;
 
 typedef std::string String;
+typedef std::vector<int> IntVector;
+typedef std::vector<thread> ThreadVector;
